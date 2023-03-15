9a27527f6d14c5243b5aa1d727f55d887c4b96cb
Donner-von-Swabia <severett21@ivytech.edu>
2023-03-15 10:26:02 -0400

    Variables
    Learning Vars. and how to use them within C++

---
+++ C++ Programming/Vars2.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+    int x = 10;
+    int y = 24;
+    int sum = x + y;
+    cout << sum << endl;
+    int a = 5, b = 6, c = 7, d = 8;
+    cout << a+b+c+d << endl;
+    /*
+    Varriable Naming convention:
+        Can contain letter, numbers, and _
+        Must begin with letter or _
+        Var. names are case sensitive
+        Do not use keywords from C++
+        No crazy characters !@#$%^&*(
+    */
+   const int con_var = 10;
+   // Cannot be changed in the program
+   
+}
